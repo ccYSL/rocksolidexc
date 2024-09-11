5224c438c8a26b088fad394b621bbed68f53592f
Ce <etodox@riseup.net>
2024-09-10 20:36:02 -0400

    Added Footer and Header to layout so all pages inherit them

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import "./globals.css";
 
 
@@ -9,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <Header />
+      {children}
+      <Footer />
+      </body>
     </html>
   );
 }
